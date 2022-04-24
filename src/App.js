791d9336c081ca00cff7c707daba6b6f8b7976ed
Arun3sh <m.aruneshwar@gmail.com>
2022-04-24 23:53:39 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { Switch, Route } from 'react-router-dom';
+import Mynavbar from './components/navbar/Mynavbar';
+import Bottomnav from './components/navbar/Bottomnav';
+import Login from './components/userpage/Login';
+import Register from './components/userpage/Register';
+import Home from './components/main/Home';
 
 function App() {
 	return (
 		<div className="App">
-			<div>Hello</div>
+			<Mynavbar />
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/register">
+					<Register />
+				</Route>
+			</Switch>
+			<Bottomnav />
 		</div>
 	);
 }
