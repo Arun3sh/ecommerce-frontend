44281526ac32616d87c0de0a51b0b96282bea20f
Arun3sh <m.aruneshwar@gmail.com>
2022-04-27 23:44:25 +0530

    footer included

--- src/App.js
+++ src/App.js
@@ -7,24 +7,44 @@ import Bottomnav from './components/navbar/Bottomnav';
 import Login from './components/userpage/Login';
 import Register from './components/userpage/Register';
 import Home from './components/main/Home';
+import Footer from './components/main/Footer';
+import { useLocation } from 'react-router-dom';
+import { useState, createContext } from 'react';
+import Productlist from './components/main/Productlist';
+
+export const myContext = createContext(null);
 
 function App() {
+	const location = useLocation();
+	const [query, setQuery] = useState('');
+	const myValue = {
+		query: query,
+		setQuery: setQuery,
+	};
+	console.log(query);
 	return (
-		<div className="App">
-			<Mynavbar />
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/login">
-					<Login />
-				</Route>
-				<Route path="/register">
-					<Register />
-				</Route>
-			</Switch>
-			<Bottomnav />
-		</div>
+		<myContext.Provider value={myValue}>
+			<div className="App">
+				{location.pathname !== '/login' && location.pathname !== '/register' && <Mynavbar />}
+
+				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/register">
+						<Register />
+					</Route>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/product">
+						<Productlist />
+					</Route>
+				</Switch>
+				{location.pathname !== '/login' && location.pathname !== '/register' && <Footer />}
+				<Bottomnav />
+			</div>
+		</myContext.Provider>
 	);
 }
 
