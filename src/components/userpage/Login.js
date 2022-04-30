1e665a092532478ce8ce416ea1f0635dca4ea8e8
Arun3sh <m.aruneshwar@gmail.com>
2022-04-30 23:54:16 +0530

    login page updated

--- src/components/userpage/Login.js
+++ src/components/userpage/Login.js
@@ -1,68 +1,66 @@
 import './Login.css';
-import {
-	Button,
-	FormControl,
-	FormControlLabel,
-	Paper,
-	Radio,
-	RadioGroup,
-	TextField,
-} from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { useHistory } from 'react-router-dom';
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import { toast } from 'react-toastify';
+import { myContext } from '../../App';
+import axios from 'axios';
+import { userApi } from '../../global';
+import loginIllu from './../../assets/eshopping.png';
+import { Link } from 'react-router-dom';
 
 toast.configure();
 
 function Login() {
-	// const { login, setLogin, value, setValue } = useContext(authContext);
+	const { setIsAuth, setCartCount, localCart, setLocalCart } = useContext(myContext);
 	const history = useHistory();
-	// const [Api, setApi] = useState(patientAPI);
 
-	// const checkUser = async () => {
-	// 	await fetch(`${Api}/login-${value}`, {
-	// 		method: 'POST',
-	// 		body: JSON.stringify(values),
-	// 		headers: {
-	// 			'Content-type': 'application/json',
-	// 		},
-	// 	})
-	// 		.then((data) => data.json())
-	// 		.then((d) => {
-	// 			localStorage.setItem('token', d.token);
-	// 			localStorage.setItem('id', d.id);
-	// 			localStorage.setItem('pt_name', d.pt_name);
-	// 			toast.success('Login success');
-	// 			setLogin(!login);
-	// 			value === 'patient' ? history.push('/appointment') : history.push('/');
-	// 		})
-	// 		.catch(() => {
-	// 			toast.error('Invalid login attempt');
-	// 		});
-	// };
+	const checkUser = () => {
+		axios
+			.post(`${userApi}/login-user`, values)
+			.then(({ data }) => {
+				localStorage.setItem('token', data.token);
+				localStorage.setItem('user_id', data.id);
+				localStorage.setItem('user_name', data.user_name);
+				localStorage.setItem('user_email', data.user_email);
+				setIsAuth(true);
+				toast.success('Log in success');
+				history.push('/');
+
+				getCart(data.id);
+			})
+			.catch((err) => toast.error(err));
+	};
+
+	const getCart = async (id) => {
+		await axios
+			.get(`${userApi}/view-cart/${id}`, {
+				headers: {
+					'x-auth-token': localStorage.getItem('token'),
+				},
+			})
+			.then(({ data }) => {
+				data.user_cart.length > 0 ? setLocalCart([...data.user_cart]) : setLocalCart([]);
+				setCartCount(data.user_cart.length);
+			});
+	};
 
 	const formValidationSchema = yup.object({
-		email: yup.string().email().required('Dont leave this field empty'),
+		user_email: yup.string().email().required('Dont leave this field empty'),
 		password: yup.string().required('Password is required'),
 	});
 
 	const { values, handleBlur, handleChange, handleSubmit, errors, touched } = useFormik({
 		initialValues: {
-			email: '',
+			user_email: '',
 			password: '',
+			data: localCart,
 		},
 		validationSchema: formValidationSchema,
-		// onSubmit: () => checkUser(),
+		onSubmit: () => checkUser(),
 	});
-
-	// To set the api value based on user selection
-	// const handleSelectChange = (event) => {
-	// 	setValue(event.target.value);
-
-	// 	event.target.value !== 'patient' ? setApi(doctorAPI) : setApi(patientAPI);
-	// };
 
 	return (
 		<div className="container-sm login-wrapper">
@@ -71,64 +69,51 @@ function Login() {
 				<p>please login</p>
 			</div>
 			<div className="login-container">
-				{/* <img className="login-img" src={docIllu} aria-label="user illustration" alt="broken" /> */}
+				<img className="login-img" src={loginIllu} aria-label="user illustration" alt="broken" />
 
 				<div className="login-container-form">
-					<Paper elevation={3} style={{ padding: '25px' }}>
-						<form className="login-form" onSubmit={handleSubmit}>
-							{/* select radio options for doctor patient login */}
-							<FormControl>
-								<RadioGroup
-									className="radioGroup-userType"
-									aria-label="type of user"
-									defaultValue="patient"
-									name="type-user"
-								>
-									<FormControlLabel value="patient" control={<Radio />} label="Patient" />
-									<FormControlLabel value="doctor" control={<Radio />} label="Doctor" />
-									<FormControlLabel value="admin" control={<Radio />} label="Admin" />
-								</RadioGroup>
-							</FormControl>
-
-							<div className="login-form-div">
-								<TextField
-									variant="standard"
-									label="email id"
-									autoFocus
-									id="email"
-									name="email"
-									value={values.email}
-									onChange={handleChange}
-									onBlur={handleBlur}
-									error={errors.email && touched.email}
-									helperText={errors.email && touched.email ? errors.email : ''}
-								/>
-								<TextField
-									variant="standard"
-									label="password"
-									type="password"
-									id="password"
-									name="password"
-									onChange={handleChange}
-									onBlur={handleBlur}
-									error={errors.password && touched.password}
-									helperText={errors.password && touched.password ? errors.password : ''}
-								/>
-								<div className="submit-user">
-									<Button
-										variant="outlined"
-										color="error"
-										// onClick={() => setValue('patient') & history.goBack()}
-									>
-										Back
-									</Button>
-									<Button variant="outlined" type="submit">
-										Submit
-									</Button>
-								</div>
+					{/* <Paper elevation={3} style={{ padding: '25px' }}> */}
+					<form className="login-form" onSubmit={handleSubmit}>
+						<div className="login-form-div">
+							<TextField
+								variant="standard"
+								label="email id"
+								autoFocus
+								id="user_email"
+								name="user_email"
+								value={values.user_email}
+								onChange={handleChange}
+								onBlur={handleBlur}
+								error={errors.user_email && touched.user_email}
+								helperText={errors.user_email && touched.user_email ? errors.user_email : ''}
+							/>
+							<TextField
+								variant="standard"
+								label="password"
+								type="password"
+								id="password"
+								name="password"
+								onChange={handleChange}
+								onBlur={handleBlur}
+								error={errors.password && touched.password}
+								helperText={errors.password && touched.password ? errors.password : ''}
+							/>
+							<div className="submit-user">
+								<Button variant="outlined" color="error" onClick={() => history.goBack()}>
+									Back
+								</Button>
+								<Button variant="outlined" type="submit">
+									Submit
+								</Button>
 							</div>
-						</form>
-					</Paper>
+						</div>
+						<div className="register-yet">
+							<Link to="/register" aria-label="not registered">
+								Haven't Registered Yet? Click here
+							</Link>
+						</div>
+					</form>
+					{/* </Paper> */}
 				</div>
 			</div>
 		</div>
