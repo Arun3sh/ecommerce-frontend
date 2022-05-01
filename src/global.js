17ee0e233f92eb4f5c9b5e7800a800dc199aac63
Arun3sh <m.aruneshwar@gmail.com>
2022-05-01 13:33:47 +0530

    heroku link added

--- src/global.js
+++ src/global.js
@@ -1,2 +1,7 @@
-export const userApi = 'http://localhost:9000/user';
-export const productApi = 'http://localhost:9000/product';
+// export const userApi = 'http://localhost:9000/user';
+// export const productApi = 'http://localhost:9000/product';
+// export const paymentApi = 'http://localhost:9000/payment';
+
+export const userApi = 'https://ecommerce-arun.herokuapp.com/user';
+export const productApi = 'https://ecommerce-arun.herokuapp.com/product';
+export const paymentApi = 'https://ecommerce-arun.herokuapp.com/payment';
